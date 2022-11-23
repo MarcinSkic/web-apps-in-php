c6c0d030d9eb63f28bb10679bb00c36f0cfa5093
MarcinSkic <marcinskicoficjal@gmail.com>
2022-11-23 11:42:55 +0100

    Finish lab4

--- Lab4/survey.php
+++ Lab4/survey.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h1>Wybierz technologie, które znasz:</h1>
-    <form action="./form.php" method="post">
+    <form action="./survey.php" method="post">
         <div class="buttons" id="service-cont">
             <?php
                 $languages = ["C","CPP","Java","C#","HTML","CSS","XML","PHP","JavaScript"];
@@ -22,6 +22,87 @@
 
     <?php
         if(filter_input(INPUT_POST,"submit")){
+            function validateSurvey(){
+                $args = [
+                    'languages' => [
+                        'filter' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+                        'flags' => FILTER_REQUIRE_ARRAY
+                    ]
+                ];
+                $data = filter_input_array(INPUT_POST,$args);
+        
+                $errors = "";
+                foreach ($data as $key => $val){
+                    if($val === false or $val === NULL){
+                        $errors .= "$key ";
+                    }
+                }
+        
+                if($errors === ""){
+                    toSurvey($data);
+                } else {
+                    echo "<br/>Nie udane głosowanie";
+                }
+            }
+        
+            function getSurveyResults(){
+                $d_root = $_SERVER['DOCUMENT_ROOT'];
+                $arrayFromFile = file("$d_root/../ExtraFiles/Lab4/survey.txt");
+                $dict = [];
+
+                foreach($arrayFromFile as $value){
+                    if(str_contains($value,"=")){
+                        $exploded = explode("=",$value);
+                        $key = $exploded[0];
+                        $value = $exploded[1];
+                        $dict[$key] = $value;
+                    }       
+                }
+
+                return $dict;
+            }
+
+            function showSurveyResults($dict){
+                echo "<h3>Wyniki ankiety: </h3>";
+                foreach ($dict as $key=>$value){
+                    echo "$key=$value<br/>";
+                }
+            }
+
+            function toSurvey($filteredData){
+                $dict = getSurveyResults();
+                
+                $newVotes = "";
+                $data = "";
+                foreach ($filteredData['languages'] as $key=>$value){
+                    $newVotes .= "$value ";
+                    if(array_key_exists($value,$dict)){
+                        $dict[$value]++;
+                    } else {
+                        $dict[$value] = 1;
+                    }
+                }
+
+                foreach ($dict as $key=>$value){
+                    $data .= "$key=$value\n";
+                }
+        
+                $d_root = $_SERVER['DOCUMENT_ROOT'];
+                $file = fopen("$d_root/../ExtraFiles/Lab4/survey.txt","w+") or die("Nie można otworzyć pliku");
+        
+                fwrite($file,$data);
+        
+                fclose($file);
+        
+                echo "<p>Zagłosowano na $newVotes</p>";
+                //Show weird look
+                //echo "<p>Zapisano: </p><br/>";
+                //echo nl2br($data);
+                
+            }
+
+            validateSurvey();
+            showSurveyResults(getSurveyResults());
             
         }
     ?>
